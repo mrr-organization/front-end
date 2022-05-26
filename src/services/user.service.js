4f0ad3c22e0efcdf5051f3e3772038f290847b19
Bazukaaa <60100309+Bazukaaa@users.noreply.github.com>
2022-05-26 23:24:22 +0700

    [Update] Ip for connect back-end

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'http://20.92.229.52:8080/api/test/';
 class UserService {
   getPublicContent() {
     return axios.get(API_URL + 'all');
