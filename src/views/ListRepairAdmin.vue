5aa2c057b2552572740b684543deec67ace25493
jednuth156 <jednuth.tatirat@mail.kmutt.ac.th>
2022-09-07 21:00:23 +0700

    [UPDATE] responsive

--- src/views/ListRepairAdmin.vue
+++ src/views/ListRepairAdmin.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mx-auto mt-24 rounded-t-lg max-w-7xl h-[760px]" style="background-color: #fef1e6">
-    <table class="w-full text-xl table-auto">
+  <div class="mx-auto sm:mt-24 mt-20 rounded-t-lg max-w-7xl sm:max-w-7xl h-[760px] sm:h-[760px] overflow-auto" style="background-color: #fef1e6">
+    <table class="w-full text-xs table-auto sm:text-xl">
       <thead>
         <tr class="bg-white ">
-          <th class="p-3 rounded-t-lg">วันที่</th>
+          <th class="p-3 rounded-t-lg sm:p-3">วันที่</th>
           <th>สถานที่ / พื่นที่</th>
           <th>ผู้แจ้ง / ผู้ร้องเรียน</th>
           <th class="rounded-t-lg">สถานะการแจ้งซ่อม / ร้องเรียน</th>
@@ -13,7 +13,7 @@
         <tr class="p-2">
           <td class="p-4">20-8-2000 18:04:02</td>
           <td>Lorem ipsum dolor </td>
-          <td>Mr. abc edwin</td>
+          <td>Mr. abc edwinx</td>
           <td>กำลังดำเนินการ</td>
         </tr>
         <tr>
--- src/views/MainAdmin.vue
+++ src/views/MainAdmin.vue
@@ -3,12 +3,12 @@
     <div class="flex flex-row justify-around w-full m-16 item-center">
       <div class="flex items-center justify-center h-96 w-96">
         <router-link to="/listrepairadmin"
-          ><img src="@/assets/listofrepairs.png" class="w-5/6 h-5/6"
+          ><img src="@/assets/listofrepairs.png" class="w-5/6 ml-4 h-5/6"
         /></router-link>
       </div>
       <div class="flex items-center justify-center h-96 w-96">
         <router-link to="/mainadmin"
-          ><img src="@/assets/repairstatus.png" class="w-5/6 h-5/6"
+          ><img src="@/assets/repairstatus.png" class="w-5/6 ml-3 h-5/6"
         /></router-link>
       </div>
     </div>
