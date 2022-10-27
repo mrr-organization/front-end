67124d22dba5b8580bc2d180fdc18a5fda8a39ff
jednuth156 <jednuth.tatirat@mail.kmutt.ac.th>
2022-10-27 16:01:34 +0700

    [Update]

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -16,12 +16,12 @@
             </ButtonCom>
         </router-link>
         <router-link to="/dashboard">
-            <ButtonCom msg="แดชบอร์ด" class="px-2 py-2 m-5 font-mono text-xl  sm:w-56 sm:p-4" 
+            <ButtonCom msg="แดชบอร์ด" class="px-2 py-2 m-5 font-mono text-xl sm:w-56 sm:p-4" 
             style="background-color: #faf0ef; color: #312a21">
             </ButtonCom>
         </router-link>
         <router-link to="/about">
-            <ButtonCom msg="เกี่ยวกับ" class="px-4 py-2 font-mono text-xl  sm:w-56 sm:p-4" 
+            <ButtonCom msg="เกี่ยวกับ" class="px-4 py-2 font-mono text-xl sm:w-56 sm:p-4" 
             style="background-color: #faf0ef; color: #312a21">
             </ButtonCom>
         </router-link>
