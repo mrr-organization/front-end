f6f0135dc5518561765ec9ee47d8c106b6eb0e7e
Bazukaaa <60100309+Bazukaaa@users.noreply.github.com>
2022-10-27 23:21:23 +0700

    [Fix] : 🛠

--- src/services/faculty.service.js
+++ src/services/faculty.service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'https://www.k-mutt-mrr-service.systems/be-path/api/faculty-service/';
 class FacultyService {
   getAllFaculty() {
-    return axios.get('http://localhost:8080/api/faculty-service/');
+    return axios.get(API_URL);
   }
   getFacultyById(id) {
     return axios.get(API_URL + 'faculty-by-id/' + id)
