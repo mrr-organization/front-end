0183b335c872aad68947c4e3fcd3d34ab4e1b579
Bazukaaa <60100309+Bazukaaa@users.noreply.github.com>
2022-10-27 14:17:52 +0700

    [UPDATE] hover

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,18 +11,15 @@
         </div>
         <div class=" sm:w-[350px] flex flex-col ml-5 justify-end pb-44 ">
         <router-link to="/login-service">
-            <ButtonCom msg="เข้าสู่ระบบ" class="px-2 py-2 font-mono text-xl sm:w-56 sm:p-4" 
-            style="background-color: #faf0ef; color: #312a21">
+            <ButtonCom msg="เข้าสู่ระบบ" class="px-2 py-2 font-mono text-xl sm:w-56 sm:p-4 hover:!bg-[#FFFFFF] transition-all !bg-[#faf0ef] !text-[#312a21]" >
             </ButtonCom>
         </router-link>
         <router-link to="/dashboard">
-            <ButtonCom msg="แดชบอร์ด" class="px-2 py-2 m-5 font-mono text-xl  sm:w-56 sm:p-4" 
-            style="background-color: #faf0ef; color: #312a21">
+            <ButtonCom msg="แดชบอร์ด" class="px-2 py-2 m-5 font-mono text-xl sm:w-56 sm:p-4 hover:!bg-[#FFFFFF] transition-all !bg-[#faf0ef] !text-[#312a21]" >
             </ButtonCom>
         </router-link>
         <router-link to="/about">
-            <ButtonCom msg="เกี่ยวกับ" class="px-4 py-2 font-mono text-xl  sm:w-56 sm:p-4" 
-            style="background-color: #faf0ef; color: #312a21">
+            <ButtonCom msg="เกี่ยวกับ" class="px-4 py-2 font-mono text-xl  sm:w-56 sm:p-4 hover:!bg-[#FFFFFF] transition-all !bg-[#faf0ef] !text-[#312a21]" >
             </ButtonCom>
         </router-link>
         </div>
