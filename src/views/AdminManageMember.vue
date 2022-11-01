557d8d9d1c797608dd0e304f6411660cd20e515a
jednuth156 <jednuth.tatirat@mail.kmutt.ac.th>
2022-11-01 22:57:15 +0700

    [UPDATE]admin manage responsive and ui

--- src/views/AdminManageMember.vue
+++ src/views/AdminManageMember.vue
@@ -1,64 +1,84 @@
 <template>
-  <div class="main-content mt-20 w-3/5 bg-white m-auto">
-    <div class="filter-bar flex justify-between">
-      <div class="search">
-        <label class="relative block">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-2 ml-2">
-            <img src="../assets/search.png" class="h-5 w-5 fill-slate-300" />
-          </span>
+  <div class="mx-auto mt-20 rounded max-w-7xl sm:max-w-7xl main-content">
+    <div class="flex justify-between filter-bar">
+      <div class="mt-2 search">
           <input
-            class="p-2 m-2 placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            class="block p-2 py-2 pr-3 m-2 text-sm border rounded-full shadow-sm pl-7 w-36 sm:w-full placeholder:italic placeholder:text-slate-400 border-slate-300 sm:pl-9 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm icon backgroud"
             type="text"
             v-model="search"
             placeholder="search anything..."
+            
           />
-        </label>
+         
+        
       </div>
-      <div class="button flex">
+      <div class="flex p-0.5 px-1 py-1 sm:px-3 my-3 mt-5 mr-2 sm:mt-2 bg-white rounded-full button ">
         <button
-          class="p-2 m-2 bg-red-400 hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-400 rounded-md"
+          class="p-0.5 m-0.5 sm:p-1 sm:m-2  rounded-md hover:bg-[#FAF0EF] active:bg-[#FF9817] focus:outline-none text-xs sm:text-sm font-semibold transition "
         >
-          <p>user</p>
+          <p>USER</p>
         </button>
+        <div class="border border-black ">
+        
+        </div>
         <button
-          class="p-2 m-2 bg-red-400 hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-400 rounded-md"
+          class="p-0.5 m-0.5 sm:p-1 sm:m-2 rounded-md hover:bg-[#FAF0EF] active:bg-[#FF9817] focus:outline-none text-xs sm:text-sm font-semibold transition "
         >
-          <p>moderator</p>
+          <p>MODERATOR</p>
         </button>
       </div>
     </div>
-    <div class="list-content mt-5">
-      <table class="table-fixed border-collapse border border-slate-400 w-full">
+    <div class=" mt-5 bg-[#FAF0EF]  rounded overflow-auto  ">
+    <div class="list-content">
+      <table class="w-full text-xs border border-collapse border-slate-400 sm:text-xl text-[#312A21] table-auto xl:table-fixed ">
         <thead>
-          <tr class="bg-white">
+          <tr class="bg-white ">
             <th class="border border-slate-300">username</th>
             <th class="border border-slate-300">ชื่อ</th>
             <th class="border border-slate-300">eamil</th>
             <th class="border border-slate-300">วันที่สมัคร</th>
-            <th class="border border-slate-300">จำนวนวันที่เป็นสมาชิก</th>
+            <th class="border border-slate-300 ">จำนวนวันที่เป็นสมาชิก</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td class="username border border-slate-300">
+            <td class="border username border-slate-300">
               <button><p>bazukaaa</p></button>
             </td>
-            <td class="full-name border border-slate-300">
+            <td class="border full-name border-slate-300">
               <button><p>poramet mongkhonmasoo</p></button>
             </td>
-            <td class="email border border-slate-300">
-              <button><p>email.er@gmail.com</p></button>
+            <td class="border email border-slate-300 ">
+              <button><p>email.er@mail.kmutt.ac.th</p></button>
             </td>
-            <td class="register-date border border-slate-300">
+            <td class="border register-date border-slate-300">
               <button><p>2022-10-2 18:20:30</p></button>
             </td>
-            <td class="count-register-date border border-slate-300">
+            <td class="border count-register-date border-slate-300">
+              <button><time>{{timeToWords('2022-11-1 18:20:30')}}</time></button>
+            </td>
+          </tr>
+          <tr>
+            <td class="border username border-slate-300">
+              <button><p>bazukaaa</p></button>
+            </td>
+            <td class="border full-name border-slate-300">
+              <button><p>poramet mongkhonmasoo</p></button>
+            </td>
+            <td class="border email border-slate-300">
+              <button><p>email.er@.kmutt.ac.th</p></button>
+            </td>
+            <td class="border register-date border-slate-300">
+              <button><p>2022-10-2 18:20:30</p></button>
+            </td>
+            <td class="border count-register-date border-slate-300">
               <button><time>{{timeToWords('2022-11-1 18:20:30')}}</time></button>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
+  </div>
     <VSPagination
       :totalPages="totalPages"
       @page-number="getListRepairNotificationByStatus"
@@ -134,4 +154,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.icon { 
+  background: url("../assets/search.png") no-repeat left scroll;
+  background-size: 20px;
+  background-position-x: 7px;
+}
+.backgroud{
+  background-color: white;
+}
+</style>
